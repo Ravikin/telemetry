7ef28ab08e970cab469ba9e651355be7023677af
ravi <ravi@IBM>
2019-09-25 00:01:39 +0200

    merging into working piece of software

--- merged.ino
+++ merged.ino
@@ -1,3 +1,6 @@
+// @TODO trzeba jeszcze sprawdzic ktory mlx z przodu jest na ktorej stronie
+
+
 #include <Wire.h>
 #include <Adafruit_MLX90614.h>
 #include <MPU6050.h>
@@ -6,7 +9,7 @@
 #include <LiquidCrystal.h>
 
 
-
+// inicjalizacja zmiennych
 MPU6050 mpu;
 MechaQMC5883 qmc;
 Adafruit_MLX90614 mlx1 = Adafruit_MLX90614(0x70);
@@ -15,7 +18,7 @@ Adafruit_MLX90614 mlx3 = Adafruit_MLX90614(0x72);
 Adafruit_MLX90614 mlx4 = Adafruit_MLX90614(0x73);
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2)
-bool b_x;
+bool b_x = True;
 
 KalmanFilter kalmanX(0.001, 0.003, 0.03);
 KalmanFilter kalmanY(0.001, 0.003, 0.03);
@@ -27,6 +30,9 @@ float accRoll = 0;
 float kalPitch = 0;
 float kalRoll = 0;
 
+// setup boarda razem z bledem nt. gyro
+// to trzeba bedzie poprawic xd @TODO
+// tak zeby mowilo ze jest problem ale sie odpalilo...no albo zeby chociaz mowilo o problemie
 void setup(){
   Serial.begin(115200);
   Serial.println("Setup");
@@ -45,22 +51,25 @@ void setup(){
   lcd.begin(16,2);
 }
 
+// i lecimy z koksem
 void loop(){
+// zbieramy dane z gyro mpu
   Vector acc = mpu.readNormalizeAccel();
   Vector gyr = mpu.readNormalizeGyro();
-
+// obliczamy wychylenia i "przyspieszenia"
   accPitch = -(atan2(acc.XAxis, sqrt(acc.YAxis*acc.YAxis + acc.ZAxis*acc.ZAxis))*180.0)/M_PI;
   accRoll  = (atan2(acc.YAxis, acc.ZAxis)*180.0)/M_PI;
- 
+ // normalizacja kalmana
   kalPitch = kalmanY.update(accPitch, gyr.YAxis);
   kalRoll = kalmanX.update(accRoll, gyr.XAxis);
 
-
+// zbieramy temperaturki
   double T1 = mlx1.readObjectTempC();
   double LT = mlx2.readObjectTempC();
   double T3 = mlx3.readObjectTempC();
   double PT = mlx4.readObjectTempC();
 
+// kierunek z magnetometru
   int x,y,z;
   qmc.read(&x,&y,&z);
 
@@ -86,7 +95,7 @@ void loop(){
   // Zamiana radianow na stopnie
   float headingDegrees = heading * 180/M_PI;
   
-
+// wypisujemy dane na serial (cos po kablu)
   Serial.print(accPitch);
   Serial.print(";");
   Serial.print(accRoll);
@@ -105,6 +114,10 @@ void loop(){
   Serial.print(";");
   Serial.println(headingDegrees);
 
+
+// wyswietlamy dane na lcdku 16 znakow na 2 wiersze
+// jezeli temperatura >=90 to niech blinka dana nazwa pozycji
+// trzeba dodac ewentualny setup gdzie wyklikujesz graniczną temperature guziczkami @TODO
   lcd.setCursor(0,0);
   if (LP >= 90)
   {
@@ -153,8 +166,11 @@ void loop(){
 	lcd.print("PT:"); lcd.print(PT);
   }
 
-  
+// zamieniamy blinkowego boola na przeciwny - mam nadzieje ze dziala xd
+// btw nie wymyslilem lepszego sposobu...ten byl pierwszy XD
+// @TODO? na pewno lepsiejszy wyswietlacz <3
   b_x = !b_x;
+  // ewentualnie delaya mozna wywalic - to po to zeby.....w sumie nie pamietam. chyba po to zeby malinka dawala rade wszystko zapisywac? na pewno jak jest za duzy delay to ktorys z elementow szalal :/ 
   delay(10);
   
 }
